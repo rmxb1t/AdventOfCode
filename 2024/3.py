f4e028a7bccc00b4fef9bf42fcdb03cb3fcb7927
rm <github.com.causing529@passmail.net>
2024-12-03 10:54:37 +0100

    Day 3: Part B done

--- 2024/3.py
+++ 2024/3.py
@@ -11,7 +11,15 @@ def partA():
     return result #profit
 
 def partB():
+    result = 0  # define result variable
+    with open('3_input.txt', 'r') as f:  # open file
+        file = f.read()  # read file
+        y = re.sub(r"don\'t\(\).*?do\(\)", "", file, flags=re.DOTALL) #remove every part from don't() - do(), flag ignores \n
+        x = re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", y)  # find every mul in the string
+        for ele in x:  # loop through each finding (list object
+            values = list(map(int, re.findall("[0-9]{1,3}", ele)))  # return only the integers, but as type int
+            result += values[0] * values[1]  # add up each multiplication result
+    return result  # profit
 
-    return
-print(partA())
-partB()
+print("Part A: ", partA())
+print("Part B: ", partB())
