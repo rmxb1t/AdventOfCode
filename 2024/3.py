f6370dea6716924e0c087f19f9b4c5b882356d22
rm <github.com.causing529@passmail.net>
2024-12-03 21:11:43 +0100

    added a short for loop

--- 2024/3.py
+++ 2024/3.py
@@ -4,10 +4,11 @@ def partA():
     result = 0 #define result variable
     with open('input/3_input.txt', 'r') as f: #open file
         file = f.read() #read file
-        x = re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", file) #find every mul in the string
-        for ele in x: #loop through each finding (list object
-            values = list(map(int, re.findall("[0-9]{1,3}", ele))) #return only the integers, but as type int
-            result += values[0] * values[1] #add up each multiplication result
+        x = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", file) #find every mul in the string
+        #for ele in x: #loop through each finding (list object
+            #values = list(map(int, re.findall("\d{1,3}", ele))) #return only the integers, but as type int
+            #result += values[0] * values[1] #add up each multiplication result
+        result = sum(int(a) * int(b) for a, b in x) #short
     return result #profit
 
 def partB():
@@ -15,10 +16,11 @@ def partB():
     with open('input/3_input.txt', 'r') as f:  # open file
         file = f.read()  # read file
         y = re.sub(r"don\'t\(\).*?do\(\)", "", file, flags=re.DOTALL) #remove every part from don't() - do(), flag ignores \n
-        x = re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", y)  # find every mul in the string
-        for ele in x:  # loop through each finding (list object
-            values = list(map(int, re.findall("[0-9]{1,3}", ele)))  # return only the integers, but as type int
-            result += values[0] * values[1]  # add up each multiplication result
+        x = re.findall("mul\((\d{1,3}),(\d{1,3})\)", y)  # find every mul in the string
+        #for ele in x:  # loop through each finding (list object
+            #values = list(map(int, re.findall("\d{1,3}", ele)))  # return only the integers, but as type int
+            #result += values[0] * values[1]  # add up each multiplication result
+        result = sum(int(a) * int(b) for a, b in x)  # short
     return result  # profit
 
 print("Part A: ", partA())
